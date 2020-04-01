0d91d2353e018f25e463d4f5e0d055825f4f888d
heecheoljeong <heecheol153@gmail.com>
2020-04-01 23:49:00 +0900

    Update script.js

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ const animation = () => {
   context.beginPath();
 
   //x軸方向に1ピクセル移動
-  context.translate(1, 0);
+  context.translate(1, 1);
 
   //描画
   context.fillRect(0, 0, rectSize, rectSize);
