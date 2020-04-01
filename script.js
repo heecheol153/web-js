4231247615c4847c7570fe528bf23446e74d192f
heecheoljeong <heecheol153@gmail.com>
2020-04-01 22:06:39 +0900

    Update script.js

--- script.js
+++ script.js
@@ -9,32 +9,35 @@ if (typeof paper.getContext === 'undefined') {
 // コンテキストを取得
 // 「2d」というのは、描画領域の種類のこと
 var context = paper.getContext('2d');
- 
+
+context.fillStyle = 'green';
+context.fillRect(10, 10, 150, 100);
+
 // Canvasの幅と高さを指定
-paper.width = 400;
-paper.height = 200;
+//paper.width = 400;
+//paper.height = 200;
 
 // スタイルは描画前に指定
 // 塗りつぶしの色を指定
-context.fillStyle = '#429955';
+//context.fillStyle = '#429955';
  
 // 正方形を描く
 // fillRect(x, y, w, h) … xyが座標、whは幅と高さ
-context.fillRect(50, 50, 100, 100);
+//context.fillRect(50, 50, 100, 100);
 
 // 緑の正方形を描画
-context.fillStyle = '#429955';
-context.fillRect(50, 50, 100, 100);
+//context.fillStyle = '#429955';
+//context.fillRect(50, 50, 100, 100);
  
 // 線を描きます宣言
-context.beginPath();
+//context.beginPath();
  
 // 線をどこから描くか宣言
-context.moveTo(240,80);
+//context.moveTo(240,80);
  
 // 線をどこまで描くか宣言
-context.lineTo(320,160);
+//context.lineTo(320,160);
  
 // 線の色を指定して、描画じゃ！！！
-context.strokeStyle = '#429955';
-context.stroke();
+//context.strokeStyle = '#429955';
+//context.stroke();
